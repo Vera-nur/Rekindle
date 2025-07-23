52b5515b1f46ece7568bb41248eab2768b07eece
Vera <135285088+Vera-nur@users.noreply.github.com>
2025-07-23 12:21:23 +0300

    firebase was established for the project

---
+++ Rekindle/RekindleTests/RekindleTests.swift
@@ -0,0 +1,17 @@
+//
+//  RekindleTests.swift
+//  RekindleTests
+//
+//  Created by Vera Nur on 23.07.2025.
+//
+
+import Testing
+@testable import Rekindle
+
+struct RekindleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
