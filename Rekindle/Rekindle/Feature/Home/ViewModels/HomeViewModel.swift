1ce557df5f1c1474ab0021c2f7847ed4fa52e426
Vera <135285088+Vera-nur@users.noreply.github.com>
2025-07-29 12:40:01 +0300

    homeviewmodel

--- Rekindle/Rekindle/Feature/Home/ViewModels/HomeViewModel.swift
+++ Rekindle/Rekindle/Feature/Home/ViewModels/HomeViewModel.swift
@@ -8,19 +8,16 @@
 import Foundation
 
 class HomeViewModel: ObservableObject {
-   // @Published var memories: [Memory] = []
+    @Published var posts: [Post] = []
 
-    init() {
-        loadSampleData()
-    }
-
-    func loadSampleData() {
-        /*memories = [
-            //deneme
-            Memory(title: "İlk Kamp", date: Date(), description: "Ormanda geçirdiğimiz o güzel gece.", imageName: "memory_icon"),
-            Memory(title: "Üniversite Mezuniyeti", date: Date(), description: "Kep atma anı, hâlâ aklımda!", imageName: "organize_icon"),
-            Memory(title: "İlk Kitabım", date: Date(), description: "Hayaller gerçek oldu!", imageName: "recall_icon")
-        ]*/
+    func loadPosts() {
+        PostService.fetchPublicPosts { [weak self] fetched in
+            DispatchQueue.main.async {
+                self?.posts = fetched
+            }
+        }
     }
 }
+
+
 
